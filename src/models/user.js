49e818d23f94e7d549fc15ca4f39581e4a751283
ROR2022 <rami.ror279@gmail.com>
2022-06-18 17:56:43 -0500

    agregando descripcion

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,9 @@ const userSchema = mongoose.Schema({
     local: {
         type: String
     },
+    description:{
+        type: String
+    },
     category:{
         type: String
     },
--- src/routes/user.js
+++ src/routes/user.js
@@ -38,10 +38,10 @@ router.get("/market/:phone",(req,res)=>{
 
 router.put('/market/:phone', (req,res)=>{
     const { phone } = req.params;
-    const { title, subtitle, local, category, servicio, diseno } = req.body;
+    const { title, subtitle, local, description, category, servicio, diseno } = req.body;
 
     userSchema
-        .updateOne({ phone: phone}, {$set: {title, subtitle, local, category, servicio, diseno}})
+        .updateOne({ phone: phone}, {$set: {title, subtitle, local, description, category, servicio, diseno}})
         .then((data)=> res.json(data))
         .catch((err)=> res.json({message: err}));
 });
